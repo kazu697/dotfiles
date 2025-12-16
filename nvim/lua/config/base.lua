bfc30cba2beed3f56c727543eff8580307ed555f
kazu697 <kazuy0697@gmail.com>
2025-12-17 00:05:39 +0900

    add neovim option

--- nvim/lua/config/base.lua
+++ nvim/lua/config/base.lua
@@ -1,2 +1,17 @@
 vim.opt.number = true;
+vim.opt.relativenumber = true;
 vim.diagnostic.config({severity_sort = true})
+
+local tabWidth = 2;
+
+vim.opt.tabstop = tabWidth;
+vim.opt.softtabstop = tabWidth;
+vim.opt.shiftwidth = tabWidth;
+
+vim.diagnostic.config({
+	virtual_text = true,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
+})
--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -27,6 +27,35 @@ require("lazy").setup({
     -- import your plugins
     -- {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"},
     -- {"prabirshrestha/vim-lsp", enabled = false},
+		{
+			"folke/lazydev.nvim",
+			ft = "lua", -- only load on lua files
+			opts = {
+				library = {
+					-- See the configuration section for more details
+					-- Load luvit types when the `vim.uv` word is found
+					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				},
+			},
+		},
+		{ -- optional blink completion source for require statements and module annotations
+			"saghen/blink.cmp",
+			opts = {
+				keymap = { preset = 'default' },
+				sources = {
+					-- add lazydev to your completion providers
+					default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+					providers = {
+						lazydev = {
+							name = "LazyDev",
+							module = "lazydev.integrations.blink",
+							-- make lazydev completions top priority (see `:h blink.cmp`)
+							score_offset = 100,
+						},
+					},
+				},
+			},
+		},
     { import = "plugins" },
   },
   -- Configure any other settings here. See the documentation for more details.
