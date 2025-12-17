37f787a32117ddd804a45f8d973da2238fc17f48
kazu697 <kazuy0697@gmail.com>
2025-12-17 18:52:46 +0900

    add: plugin

---
+++ nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		'akinsho/toggleterm.nvim', 
+		version = "*", 
+		opts = {
+			--[[ things you want to change go here]]
+			open_mapping = "<Leader>tt",
+			direction = "float",
+			float_opts = {
+				border = "single",
+			},
+		}
+	}
+}
