6b72e3dbe31c8c445ca3ba64dce453e9ae56fbd6
kazu697 <kazuy0697@gmail.com>
2025-12-18 18:49:47 +0900

    feat: add plugin

---
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = { "go", "lua" }, -- Go と Lua を Treesitter 対応させる
+      highlight = {
+        enable = true,  -- Tree-sitter でのハイライトを有効化
+      },
+    })
+  end,
+}
