9cee85aa0788679853db622eb8e8df630715e2fe
kazu697 <kazuy0697@gmail.com>
2025-12-18 23:04:29 +0900

    add: plugin

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -33,8 +33,17 @@ return {
 		},
     quickfile = { enabled = true },
     scope = { enabled = true },
+		terminal = {
+			enabled = true,
+			win = {
+				position = "float",
+				border = "single",
+				width = 0.8,
+				height = 0.6,
+			}
+		},
     scroll = {
-			enabled = false,
+			enabled = true,
 			  animate = {
 					duration = { step = 10, total = 200 },
 					easing = "linear",
@@ -56,7 +65,11 @@ return {
 						notification = {
 							-- wo = { wrap = true } -- Wrap notifications
 						}
-    }
+    },
+		toggle = {
+			map = vim.keymap.set,
+			which_key = true,
+		},
   },
   keys = {
     -- Top Pickers & Explorer
@@ -122,8 +135,6 @@ return {
     { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
     { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
     -- Other
-    { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
-    { "<leader>Z",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
     { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
     { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
     { "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
@@ -136,6 +147,10 @@ return {
     { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
     { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
     { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
+		-- Terminal
+		{
+			"<leader>t", function() Snacks.terminal.toggle() end, desc = "Toggle Snacks Floating Terminal"
+		}
   },
   init = function()
     vim.api.nvim_create_autocmd("User", {
@@ -155,15 +170,13 @@ return {
             dd(...)
           end
         else
-          vim.print = _G.dd 
+          vim.print = _G.dd
         end
 
         -- Create some toggle mappings
-        Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
         Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
         Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
         Snacks.toggle.diagnostics():map("<leader>ud")
-        Snacks.toggle.line_number():map("<leader>ul")
         Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
         Snacks.toggle.treesitter():map("<leader>uT")
         Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
