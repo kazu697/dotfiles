4f17768eb93a6a7e8655c92dcb26b731c4e93a3e
kazu697 <kazuy0697@gmail.com>
2025-12-18 23:36:21 +0900

    fix: dashboard

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -12,7 +12,28 @@ return {
       },
     },
     bigfile = { enabled = true },
-    dashboard = { enabled = true },
+    dashboard = {
+			enabled = true,
+			sections = {
+				{ section = "header" },
+				{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+				{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+				{
+					icon = " ",
+					title = "Git Status",
+					section = "terminal",
+					enabled = function()
+						return Snacks.git.get_root() ~= nil
+					end,
+					cmd = "git status --short --branch --renames",
+					-- height = 5,
+					-- padding = 1,
+					-- ttl = 5 * 60,
+					-- indent = 3,
+				},
+				{ section = "startup" },
+			},
+		},
     explorer = {
 			enabled = true,
 			hidden = true,
@@ -40,6 +61,7 @@ return {
 				border = "single",
 				width = 0.8,
 				height = 0.6,
+				winblend = 30,
 			}
 		},
     scroll = {
