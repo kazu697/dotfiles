86b3e50884f38c808bad95d3528533bba1beb97b
kazu697 <kazuy0697@gmail.com>
2025-12-22 22:19:10 +0900

    fix mise config

--- install.sh
+++ install.sh
@@ -2,4 +2,11 @@
 
 set -e
 
-ln -fns "$(pwd)/nvim" "${HOME}/.config/nvim"
+export DOTFILES_DIR="$(pwd)"
+
+ln -fns "${DOTFILES_DIR}/nvim" "${HOME}/.config"
+ln -fns "${DOTFILES_DIR}/mise" "${HOME}/.config"
+
+# install mise
+curl https://mise.run | sh
+echo 'eval "$(~/.local/bin/mise activate bash)"' >> ~/.zshrc
--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -24,6 +24,7 @@ return {
 		explorer = {
 			enabled = true,
 			hidden = true,
+			ignored = true,
 		},
 		indent = { enabled = true },
 		input = { enabled = true },
@@ -36,6 +37,7 @@ return {
 			source = {
 				explorer = {
 					hidden = true,
+					ignored = true,
 				},
 			},
 		},
@@ -288,7 +290,7 @@ return {
 				Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
 				Snacks.toggle.diagnostics():map("<leader>ud")
 				Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map(
-				"<leader>uc")
+					"<leader>uc")
 				Snacks.toggle.treesitter():map("<leader>uT")
 				Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
 				Snacks.toggle.inlay_hints():map("<leader>uh")
