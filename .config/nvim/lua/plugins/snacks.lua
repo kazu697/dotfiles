6b925ed6c43c910d87ac9c2f3927234d51e5de71
kazu697 <kazuy0697@gmail.com>
2026-01-10 22:11:32 +0900

    fix

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -14,7 +14,15 @@ return {
     },
   },
   init = function()
-    -- 隠しファイルの色を通常と同じにする
-    vim.api.nvim_set_hl(0, "SnacksPickerPathHidden", { link = "Normal" })
+    -- 隠しファイル/ignoredファイルの色を通常と同じにする
+    local function set_snacks_hl()
+      vim.api.nvim_set_hl(0, "SnacksPickerPathHidden", { link = "Normal" })
+      vim.api.nvim_set_hl(0, "SnacksPickerPathIgnored", { link = "Normal" })
+    end
+    set_snacks_hl()
+    -- カラースキーム変更時にも再適用
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      callback = set_snacks_hl,
+    })
   end,
 }
